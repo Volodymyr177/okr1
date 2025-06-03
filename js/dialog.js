9197f6d7dbf63975ab5147258e732b965b388107
Volodymyr177 <127132327+Volodymyr177@users.noreply.github.com>
2025-06-03 17:02:40 +0300

    update

--- js/dialog.js
+++ js/dialog.js
@@ -1,29 +1,29 @@
-function userDialog() {
-    let name = prompt("Як вас звати?", "Гість");
-    let age = prompt("Скільки вам років?");
-    if (age >= 18) {
-        alert(`Вітаю, ${name}! Вам ${age} років — ви повнолітній.`);
-    } else {
-        alert(`Привіт, ${name}! Вам лише ${age} — ще не повнолітній.`);
-    }
-    if (confirm("Хочете побачити щось цікаве?")) {
-        alert("🗓 Подивіться ваш розклад!");
-    } else {
-        alert("До зустрічі!");
-    }
-}
-
-userDialog();
-
-function showDeveloperInfo(surname, name, position = "PHP Developer") {
-    alert(`Розробник: ${surname} ${name}\\nПосада: ${position}`);
-}
-
-showDeveloperInfo("Кузьменко", "Володимир");
-
-const compareStrings = (a, b) => {
-    if (a.length > b.length) alert(`Довший рядок: ${a}`);
-    else if (a.length < b.length) alert(`Довший рядок: ${b}`);
-    else alert("Рядки однакової довжини.");
-};
-compareStrings(prompt("1 рядок:"), prompt("2 рядок:"));
+// function userDialog() {
+//     let name = prompt("Як вас звати?", "Гість");
+//     let age = prompt("Скільки вам років?");
+//     if (age >= 18) {
+//         alert(`Вітаю, ${name}! Вам ${age} років — ви повнолітній.`);
+//     } else {
+//         alert(`Привіт, ${name}! Вам лише ${age} — ще не повнолітній.`);
+//     }
+//     if (confirm("Хочете побачити щось цікаве?")) {
+//         alert("🗓 Подивіться ваш розклад!");
+//     } else {
+//         alert("До зустрічі!");
+//     }
+// }
+//
+// userDialog();
+//
+// function showDeveloperInfo(surname, name, position = "PHP Developer") {
+//     alert(`Розробник: ${surname} ${name}\\nПосада: ${position}`);
+// }
+//
+// showDeveloperInfo("Кузьменко", "Володимир");
+//
+// const compareStrings = (a, b) => {
+//     if (a.length > b.length) alert(`Довший рядок: ${a}`);
+//     else if (a.length < b.length) alert(`Довший рядок: ${b}`);
+//     else alert("Рядки однакової довжини.");
+// };
+// compareStrings(prompt("1 рядок:"), prompt("2 рядок:"));
